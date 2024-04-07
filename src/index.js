040821e4bf7eb837af97ea31f2b23ffa17a5c85a
Invisij <dotuanhung41@gmail.com>
2024-04-07 07:45:11 +0700

    test

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import '~/index.css';
 import App from '~/App';
 import reportWebVitals from '~/reportWebVitals';
+import GlobalStyles from './components/GlobalStyles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <Router>
-            <App />
+            <GlobalStyles>
+                <App />
+            </GlobalStyles>
         </Router>
     </React.StrictMode>,
 );
